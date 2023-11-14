e13f6d5a3ed1a786240b100c43bf480ac2a3032d
Alex <mike123777mike@gmail.com>
2023-11-15 02:14:07 +0300

    hw3 second part

--- app/management/commands/fill_db.py
+++ app/management/commands/fill_db.py
@@ -9,10 +9,10 @@ fake = Faker()
 
 def random_choice(arr, length):
     answer = []
-    i = fake.random_int(min=0, max=length // 10)
+    i = fake.random_int(min=0, max=length // 7)
     while i < length:
         answer.append(arr[i])
-        i += fake.random_int(min=1, max=length // 10)
+        i += fake.random_int(min=1, max=length // 7)
     return answer
 
 
@@ -33,78 +33,93 @@ class Command(BaseCommand):
                 last_login=fake.date_time_between(start_date='-1y', end_date='+1y').astimezone(dt.timezone.utc)
             ) for i in range(num)
         ]
+        print("users")
         User.objects.bulk_create(users)
         users = User.objects.all()
+        print("users_bulk")
 
         profiles = [
             Profile(
                 user=users[i],
-                avatar=(str(i), None)[fake.random_int(min=0, max=1)]
+                avatar=str(i),
+                points=fake.random_int(min=0, max=100000)
+            ) if fake.boolean() else
+            Profile(
+                user=users[i],
+                points=fake.random_int(min=0, max=100000)
             ) for i in range(num)
         ]
+        print("profiles")
         Profile.objects.bulk_create(profiles)
         profiles = Profile.objects.all()
         profiles_count = profiles.count()
+        print("profiles_bulk")
 
         tags = [
             Tag(
-                name=fake.word()
+                name=fake.word(),
+                mentions=fake.random_int(min=0, max=num*10)
             ) for _ in range(num)
         ]
+        print("tages")
         Tag.objects.bulk_create(tags)
         tags = Tag.objects.all()
         tags_count = tags.count()
+        print("tages_bulk")
 
         questions = [
             Question(
                 author=profiles[fake.random_int(min=0, max=profiles_count - 1)],
                 date_time=fake.date_time_between(start_date='-3y', end_date='-1y').astimezone(dt.timezone.utc),
                 title=fake.sentence().replace('.', ''),
-                text=fake.text()
+                text='\n'.join((fake.text(), fake.text(), fake.text())),
+                num_likes=fake.random_int(min=0, max=num*100),
+                num_dislikes=fake.random_int(min=0, max=num*100),
+                solved=fake.boolean()
             ) for _ in range(num * 10)
         ]
+        print("questions")
         Question.objects.bulk_create(questions)
         questions = Question.objects.all()
-        for question in questions:
+        questions_count = questions.count()
+        print("questions_bulk")
+        for i, question in enumerate(questions):
             question.tagquestion.add(*random_choice(tags, tags_count))
             question.likes.add(*random_choice(profiles, profiles_count))
             question.dislikes.add(*random_choice(profiles, profiles_count))
-        questions_count = questions.count()
+            if i % 100 == 0:
+                print("questions_" + str(i))
 
         answers = [
             Answer(
                 question=questions[fake.random_int(min=0, max=questions_count - 1)],
                 author=profiles[fake.random_int(min=0, max=profiles_count - 1)],
                 date_time=fake.date_time_between(start_date='-1y', end_date='+1y').astimezone(dt.timezone.utc),
-                text=fake.sentences()
+                text=fake.text(),
+                is_solving=fake.boolean()
             ) for _ in range(num * 100)
         ]
+        print("answers")
         Answer.objects.bulk_create(answers)
+        print("answers_bulk")
 
-
-        # TagQuestion = Tag.rated_by.through
-        # tagquestions = [
-        #     TagQuestion(
-        #         tag=tags[fake.random_int(min=0, max=tags_count - 1)],
-        #         question=questions[fake.random_int(min=0, max=questions_count - 1)]
-        #     ) for _ in range(num * 20)
-        # ]
-        # TagQuestion.objects.bulk_create(tagquestions)
-        #
-        # Likes = Profile.rated_by.through
-        # likes = [
-        #     Likes(
-        #         profile=profiles[fake.random_int(min=0, max=profiles_count - 1)],
-        #         question=questions[fake.random_int(min=0, max=questions_count - 1)]
-        #     ) for _ in range(num * 100)
-        # ]
-        # Likes.objects.bulk_create(likes)
-        #
-        # Dislikes = Question.rated_by.through
-        # dislikes = [
-        #     Dislikes(
-        #         profile=profiles[fake.random_int(min=0, max=profiles_count - 1)],
-        #         question=questions[fake.random_int(min=0, max=questions_count - 1)]
-        #     ) for _ in range(num * 100)
-        # ]
-        # Dislikes.objects.bulk_create(dislikes)
+        names = ["Высший разум", "Искусственный Интеллект", "Гений", "Оракул", "Просветленный", "Мудрец", "Мыслитель",
+                 "Гуру", "Мастер", "Профи", "Знаток", "Ученик", "Новичок"][::-1]
+        ratings = [
+            Rating(
+                minimum=0,
+                name=names[0]
+            )
+        ]
+        n = 50.0
+        for i in range(1, 13):
+            ratings.append(
+                Rating(
+                    minimum=round(n),
+                    name=names[i]
+                )
+            )
+            n *= 1.873818  # максимальное получаемое число 50000
+        print("ratings")
+        Rating.objects.bulk_create(ratings)
+        print("ratings_bulk")
--- app/models.py
+++ app/models.py
@@ -2,20 +2,56 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class ProfileManager(models.Manager):
+    def popular(self):
+        return self.annotate(type=Rating.objects.get_rating(models.F('points'))).order_by('-count')[:10]
+
+    def get_user(self, qobj):
+        return qobj.author.annotate(type=Rating.objects.get_rating(models.F('points')))
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    avatar = models.CharField(max_length=32, null=True, default=None)
+    avatar = models.CharField(max_length=32, null=True)
     points = models.IntegerField(default=0)
+
+    objects = ProfileManager()
+
+
+class TagManger(models.Manager):
+    def popular(self):
+        return self.annotate(count=Question.objects.filter(Tags__id=models.F('id')).count()
+                             ).order_by('-count')[:10].values_list('name', flat=True)
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=32)
     mentions = models.IntegerField(default=0)
 
+    objects = TagManger()
+
+    def __str__(self):
+        return str(self.name)
+
+
+class QuestionManager(models.Manager):
+    def sorted_by_date(self):
+        return self.order_by("-date_time")
+
+    def sorted_by_likes(self):
+        return self.order_by("-likes", "-date_time")
+
+    def with_tag(self, name):
+        tag_id = Tag.objects.get(name=name).id
+        return self.filter(Tags__id=tag_id).order_by("-date_time")
+
+    def get_question(self, qid):
+        return self.get(id=qid)
+
 
 class Question(models.Model):
     author = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    tagquestion = models.ManyToManyField(Tag, related_name="TagQuestion")
+    tags = models.ManyToManyField(Tag, related_name="Tags")
     date_time = models.DateTimeField()
     title = models.CharField(max_length=512)
     text = models.TextField(null=True)
@@ -23,7 +59,15 @@ class Question(models.Model):
     likes = models.ManyToManyField(Profile, related_name="Likes")
     num_dislikes = models.IntegerField(default=0)
     dislikes = models.ManyToManyField(Profile, related_name="Dislikes")
-    solve = models.BooleanField(default=False)
+    answers = models.IntegerField(default=0)
+    solved = models.BooleanField(default=False)
+
+    objects = QuestionManager()
+
+
+class AnswerManager(models.Manager):
+    def of_question(self, qobj):
+        return self.filter(question=qobj).annotate(user=models.F("author").user).order_by("-date_time")
 
 
 class Answer(models.Model):
@@ -33,7 +77,16 @@ class Answer(models.Model):
     text = models.TextField()
     is_solving = models.BooleanField(default=False)
 
+    objects = AnswerManager()
+
+
+class RatingManager(models.Model):
+    def get_rating(self, points):
+        return self.filter(minimum__gt=points).order_by('-minimum')[0].name
+
 
 class Rating(models.Model):
     minimum = models.IntegerField()
     name = models.CharField(max_length=32)
+
+    objects = RatingManager()
